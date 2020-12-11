a0b13df60c17f3ff08949f5610d31c742e98ba3e
gabriel_sb10 <gbelem4@hotmail.com>
2020-12-11 18:22:40 -0300

    ignore corrigido

---
+++ livrusp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Cad_venda)
+
+admin.site.register(Cad_compra)
---
+++ livrusp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("register/", views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
+    path('busca/', views.busca, name="busca"),
+    path('cadastrar_livro_venda/', views.cad_venda, name="cad_venda"),
+    path('cadastrar_livro_compra/', views.cad_compra, name="cad_compra")
+]
